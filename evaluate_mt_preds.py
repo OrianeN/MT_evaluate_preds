657c9f33cc2c930c7195e537a88028b180a7de4d
OrianeN <oriane.nedey@inria.fr>
2024-07-26 16:08:37 +0200

    Compute and store scores for each segment (+ stats)
    + add docstring

--- evaluate_mt_preds.py
+++ evaluate_mt_preds.py
@@ -2,42 +2,92 @@
 in cases where you don't have access to the model (or even for human translation),
 but only the predictions (+ references/sources)
 
+requires numpy package: `pip install numpy`
+
 Usage: `python3 evaluate_mt_preds.py -s source_file.txt -p pred_file.txt -r ref_file.txt`
 
 Metrics implemented: BLEU, COMET
 """
 
 import argparse
+from collections import defaultdict
 
+import numpy as np
 from comet import download_model as download_comet_model
 from comet import load_from_checkpoint as load_comet_from_checkpoint
 from sacrebleu.metrics import BLEU
 
 
 def compute_metrics(predictions, references, sources, comet_model):
-    metrics = {}
+    """
+    Compute scores given a triplets with predictions, references and sources.
 
-    # Compute BLEU score
-    metrics["BLEU"] = BLEU().corpus_score(predictions, [references]).score
+    Metrics used are BLEU and COMET (only if a model is passed).
+
+    Computes scores at the sentence level, and returns both the global and sentence-specific scores.
+    For BLEU, this means computing the scores twice, with `.corpus_score` and `sentence_score`.
+
+    :param predictions: list of predicted translations
+    :param references: list of reference translations
+    :param sources: list of source segments
+    :param comet_model: loaded COMET model
+    :return:
+    - dict with global scores for each metric
+    - dict with scores for each sentence and for each metric,
+        in the format {"BLEU": [score1, score2...], "COMET": [score1, score2...]}
+    """
+    metrics_global = {}
+    metrics_sentences = defaultdict(list)
+
+    # Compute BLEU score at corpus-level
+    metrics_global["BLEU"] = BLEU().corpus_score(predictions, [references]).score
+
+    # Compute BLEU score for each sentence
+    bleu_metric = BLEU(effective_order=True)
+    for pred, ref in zip(predictions, references):
+        metrics_sentences["BLEU"].append(bleu_metric.sentence_score(pred, [ref]).score)
 
     # Compute COMET score
     if comet_model:
-        comet_score = comet_model.predict([
+        comet_result = comet_model.predict([
             {"src": src_seg, "mt": mt_seg, "ref": ref_seg}
             for src_seg, mt_seg, ref_seg
             in zip(sources, predictions, references)
-        ]).system_score
-        metrics["COMET"] = 100 * comet_score
+        ])
+        metrics_global["COMET"] = 100 * comet_result.system_score
+        metrics_sentences["COMET"] = [100 * s for s in comet_result.scores]
 
-    return metrics
+    return metrics_global, metrics_sentences
+
+
+def print_sentence_scores_stats(sent_scores):
+    """
+    Prints detailed stats for the sentence-specific scores of each metric
+    :param sent_scores: dict of format {"BLEU": [score1, score2, ...], "COMET": [score1, score2, ...]}
+    """
+    for metric_name, metric_scores in sent_scores.items():
+        print(f"\nStats for {metric_name}:")
+        np_scores = np.asarray(metric_scores)
+        print(f"\t-Mean: {np_scores.mean():.4f}")
+        print(f"\t-Median: {np.median(np_scores):.4f}")
+        print(f"\t-Min: {np_scores.min():.4f}")
+        print(f"\t-Max: {np_scores.max():.4f}")
+        print(f"\t-Standard deviation: {np.std(np_scores):.4f}")
 
 
 def load_segments_from_file(path):
+    """
+    Loads segments from a TXT file, where each line contains one segment
+    :return: list of segments
+    """
     with open(path) as f:
         return [l.strip() for l in f]
 
 
 def load_comet_model(model_name):
+    """
+    (Downloads and) loads COMET model
+    """
     comet_model_path = download_comet_model(model_name)
     return load_comet_from_checkpoint(comet_model_path)
 
@@ -53,6 +103,9 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--ref_file",
                         help="Path to the file with reference translations",
                         required=True)
+    parser.add_argument("-o", "--output_triplet_scores",
+                        help="Path to the output TSV file where to write sentence triplets with their scores",
+                        default="output_triplet_scores.tsv")
     parser.add_argument("-cm", "--comet_model_name",
                         help="Name of the COMET model to run evaluation with",
                         default="Unbabel/wmt22-comet-da")
@@ -70,8 +123,26 @@ if __name__ == "__main__":
     preds_segments = load_segments_from_file(args.pred_file)
     ref_segments = load_segments_from_file(args.ref_file)
 
-    results = compute_metrics(preds_segments, ref_segments, source_segments, comet_model)
+    results_global, results_sentences = compute_metrics(preds_segments, ref_segments, source_segments, comet_model)
 
+    # Write results to stdout
     print("\nResults:")
-    for metric, score in results.items():
+    for metric, score in results_global.items():
         print(f"\t- {metric}: {score:.2f}")
+    print_sentence_scores_stats(results_sentences)
+
+    # Export scores and triplets into a TSV file
+    with open(args.output_triplet_scores, "w", encoding="utf-8") as f:
+        # Write header
+        for metric_name in results_sentences.keys():
+            f.write(metric_name+"\t")
+        f.write("Source segment\tPredicted translation\tReference translation\n")
+        # Write all triplets along with their scores
+        for i, (src_seg, pred_seg, ref_seg) in enumerate(zip(source_segments, preds_segments, ref_segments)):
+            # Write scores
+            for metric_scores in results_sentences.values():
+                f.write(f"{metric_scores[i]:.4f}\t")
+            # Write triplet
+            f.write(f"{src_seg}\t{pred_seg}\t{ref_seg}\n")
+
+    print(f"\nSentence-level scores stored in {args.output_triplet_scores}")
